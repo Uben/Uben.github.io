aeddf626212062f776426ef9584803ce02dfa2c4
Bernin Alejandro Uben <berninuben@icloud.com>
2017-06-03 21:28:03 -0400

    Added Google Analytics, Updated Header, removed fonts, remade simple 'tab' icon, moved navigation jquery to bottom

--- js/navigation.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$('.menu-burger').click( function() {
-		$('.bar').toggleClass('animate'); // Creates the Hamburger -> X Animation on first click
-		$('.overlay').fadeToggle(100, 'linear'); // toggles open the nav overlay // Optionally could change fadeIn & fadeut to both toggleClass
-		$('.menu-options-list').delay(100).addClass('slideDownIn');	// Rapidly fades option in upward into place
-	});
-});
