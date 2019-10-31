40ee9562e622ef6e60cc6b8b2ad021a61f6cc3fd
Bernin Alejandro Uben <berninuben@icloud.com>
2019-10-31 01:09:15 -0400

    added work status to index page && more spacing between nav items

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,9 +18,12 @@ class Home extends React.PureComponent {
 					<p className="title"> software developer </p>
 					<div className="divider" />
 					<p className="about"> Yo. Im a Software Developer, self-taught Designer, & undergraduate Computer Science student living in New Jersey. If you want to know more about me or if youʼre a Nigerian prince who wants to offer me a lot of money, feel free to contact me. This is my place within the IPv4 space. </p>
+					<div className="work-status">
+						<p> Currently <span> open </span> to new opportunities. </p>
+					</div>
 
 					<div id="social">
-						<Link className="icon" to="#">
+						<Link className="icon" to="/">
 							<img src={mailSVG} alt="mail icon"/>
 						</Link>
 						<a className="icon" href="https://github.com/uben" target="_blank">
