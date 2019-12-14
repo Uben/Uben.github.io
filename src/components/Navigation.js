f95fdd267a5c0b5704014d7875fef1ee2ffb4df4
Bernin Alejandro Uben <berninuben@icloud.com>
2019-12-14 09:55:50 -0500

    navigation style updates, avoiding use of vh for .site-outline in mobile broswers

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -12,9 +12,13 @@ class Navigation extends React.PureComponent {
 						<h5 className="logo"> uben </h5>
 					</Link>
 					<ul>
+						<li className="nav-divider">x</li>
 						<li> <Link to="/work"> work </Link> </li>
+						<li className="nav-divider">x</li>
 						<li> <Link to="/experience"> experience </Link> </li>
+						{/* <li className="nav-divider">x</li> */}
 						{/* <li> <Link to="/blog"> blog </Link> </li> */}
+						{/* <li className="nav-divider">x</li> */}
 						{/* <li> <Link to="/"> contact </Link> </li> */}
 					</ul>
 				</div>
